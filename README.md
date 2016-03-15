cb622408c133d3bb927096e724ebff707f130d06
= <hello@imsean.me>
2016-03-14 22:26:25 -0500

    Got some front end done.

---
+++ README.md
@@ -0,0 +1,17 @@
+# synet
+> Synet website
+
+## Installation
+```shell
+$ git clone && cd synet
+$ npm install
+$ npm start
+```
+
+## Credits
+| ![seanc][avatar] |
+|:---:|
+| [Sean Wilson][github] |
+
+  [avatar]: https://avatars.githubusercontent.com/u/13725538?v=3&s=125
+  [github]: https://github.com/seanc
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const gulp = require('gulp'),
+  babel = require('gulp-babel'),
+  sass = require('gulp-sass'),
+  prefixer = require('gulp-autoprefixer');
+  
+const opts = {
+  autoPrefixer: {
+    browsers: ['last 2 versions'],
+    cascade: false
+  },
+  babel: {
+    presets: ['es2015']
+  }
+};
+
+const paths = {
+  sass: './public/src/styles/**/*.scss',
+  scripts: './public/src/scripts/**/*.js'
+};
+  
+gulp.task('sass:build', () => 
+  gulp.src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefixer(opts.autoPrefixer))
+    .pipe(gulp.dest('./public/dist/styles'))
+);
+
+gulp.task('javascript:build', () =>
+  gulp.src(paths.scripts)
+    .pipe(babel(opts.babel))
+    .pipe(gulp.dest('./public/dist/scripts'))
+);
+
+gulp.task('default', ['sass:build', 'javascript:build']);
+
+gulp.task('watch', () => {
+  gulp.watch(paths.sass, ['sass:build']);
+  gulp.watch(paths.scripts, ['javascript:build']);
+});
